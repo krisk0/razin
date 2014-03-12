579168d6136b0be08a98349378bd70f010c7f196
Денис Крыськов <qauqe@list.ru>
2014-03-12 20:03:52 +0400

    implemented solve_right

--- flint.binding/flint.pyx
+++ flint.binding/flint.pyx
@@ -12,6 +12,9 @@
 #                  http://www.gnu.org/licenses/
 ###########################################################################
 
+# I am adding new functions to this file. Only functions I personally need
+# Maybe To.do file will give a clue, on what and why I am doing
+
 from sage.rings.integer cimport Integer
 from sage.matrix.matrix_integer_dense cimport Matrix_integer_dense
 
@@ -37,6 +40,9 @@ cdef extern from 'flint/fmpz_mat.h':
  void fmpz_mat_print(fmpz_mat_t m)
  void fmpz_mat_clear(fmpz_mat_t m)
  void fmpz_mat_init(fmpz_mat_t m, long rows, long cols)
+ int fmpz_mat_solve(fmpz_mat_t X, fmpz_t den, const fmpz_mat_t A, const 
+  fmpz_mat_t B)
+ int fmpz_mat_equal(const fmpz_mat_t a,const fmpz_mat_t b)
 
 cdef class fmpz_mat:
 
@@ -74,7 +80,46 @@ cdef class fmpz_mat:
    fmpz_get_mpz( t.value, self.matr[0].entries+i )
    r.append( int(t) )
   return r
- 
+  
+ def solve_right_slave(self,fmpz_mat B):
+  cdef fmpz_mat Xd
+  Xd = fmpz_mat.__new__( fmpz_mat )
+  fmpz_mat_init( Xd.matr, B.matr[0].r, B.matr[0].c )
+  cdef Integer d_sage
+  cdef fmpz_t d
+  fmpz_init( d )
+  d_sage = Integer(0)
+  if fmpz_mat_solve( Xd.matr, d, self.matr, B.matr ):
+   fmpz_get_mpz( d_sage.value, d )
+   fmpz_clear( d )
+   return Xd, d_sage
+  fmpz_clear( d )
+  fmpz_mat_clear( Xd.matr )
+  return None,d_sage
+
+ def solve_right(self,B):
+  '''
+   A=self 
+   B is fmpz_mat or smth valid for fmpz_mat constructor
+   Solve A*X=B for X. Return pair (Xd,d) where Xd is integer matrice
+   If A is singular, return (None,0)
+   
+   B must have proper dimensions
+  '''
+  if isinstance(B,fmpz_mat):
+   return self.solve_right_slave(B)
+  C=fmpz_mat(B)
+  return self.solve_right_slave(C)
+
+ def __richcmp__(fmpz_mat a, fmpz_mat b, int op):
+  cdef bint r
+  if op != 2 and op != 3:
+   raise TypeError('fmpz_mat.__richcmp__(): <> relation not defined')
+  r = fmpz_mat_equal( a.matr, b.matr )
+  if op == 3:
+   r = not r
+  return r
+
  def __dealloc__(self):
   fmpz_mat_clear(self.matr)
 
