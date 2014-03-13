c9bab8bae7e83c648c94241652801462c189653d
Денис Крыськов <qauqe@list.ru>
2014-03-13 19:06:25 +0400

    cleaned code, added the ability to watch for non-canonical matrix

--- flint.binding/test_Dixon_square.py
+++ flint.binding/test_Dixon_square.py
@@ -4,6 +4,13 @@
 # This program is part of RAZIN
 # Copyright Денис Крыськов 2014
 
+'''
+Test wrappers for fmpz_mat_solve, fmpq_mat_solve_dixon, fmpq_mat_inv
+
+if watch_canonical=1, then print some information if raw_str() returns different
+ information (which can only happen when one of the is not in canonical form)
+'''
+
 import sage.all
 import flint_sage as flint
 import sys
@@ -11,13 +18,11 @@ import sys
 ZZ=sage.all.ZZ
 write=sys.stdout.write
 Integer=sage.all.Integer
-dim0=10
+dim0=15
+watch_canonical=1
 
 def random_matrice(dim):
  return sage.all.random_matrix( ZZ, dim, x=-100, y=100 )
-
-def sage_ZZ_to_flint_QQ( d, a ):
- return flint.fmpq_mat( (Integer(d), flint.fmpz_mat( a ) ) )
 
 def test_with( m ):
  i=sage.all.identity_matrix( m.nrows() )
@@ -43,6 +48,20 @@ def test_with( m ):
  assert X == Y
  assert Y == W
  write('+')
+ if watch_canonical:
+  ' check if raw_str() of X, Y and W differ '
+  Xr,Yr,Wr=X.raw_str(),Y.raw_str(),W.raw_str()
+  m_printed=0
+  if Xr != Yr:
+   print 'm=\n',m
+   print 'Xr=%s' % Xr
+   print 'Yr=%s' % Yr
+   m_printed=1
+  if Yr != Wr:
+   if not m_printed:
+    print 'm=\n',m
+   print 'Yr=%s' % Yr
+   print 'Wr=%s' % Wr
 
 def singular_test( mZ, mQ, iZ, iQ ):
  drop,zero=mZ.solve_right(iZ)
@@ -55,14 +74,14 @@ def singular_test( mZ, mQ, iZ, iQ ):
 a=sage.all.matrix( 2, [5, 5, 7, 8] )
 test_with( a )
 a[1,0],a[1,1]=-5,-5
-#print 'Singular matrice:\n',a
 test_with( a )
 
-sys.stdout.write('\n+')
+sys.stdout.write('\n')
 for i in range(10):
  dim=dim0 + 10*i
- a=random_matrice( dim )
- test_with( a )
+ for j in range(7):
+  a=random_matrice( dim )
+  test_with( a )
  for j in range(dim):
   a[1,j]=0
  test_with( a )
