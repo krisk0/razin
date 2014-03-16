080681bc7c56723fd94b38d01f33ae37b095d21a
Денис Крыськов <qauqe@list.ru>
2014-03-16 10:37:23 +0400

    nmod_mat_HNF() specification

--- flint.binding/nmod_mat_HNF.c
+++ flint.binding/nmod_mat_HNF.c
@@ -7,15 +7,19 @@ nmod_mat_HNF(nmod_mat_t A)
 /*
 HNF: Hermite Normal Form as defined by W.Stein/C.Pernet
 
-A: non-singular over integers, det(A) is a divisor of n=mod
+Input data:
+~~~~~~~~~~
+A: square non-singular over integers, A.r>0, det(A) is a divisor of n=mod
+
 let H be HNF of A over Z/nZ
 
+Output data:
+~~~~~~~~~~~
 if H is non-singular, modify A to be equal H and return -1
 if H is singular, return j such that H[j,j] is zero, and return A with
  the property
-
- 0 <= A[i,j] < n,  A[i,j] = H[i,j] modulo n for i<j
- (all other entries except j-1 defined by line above undefined)
+ for all i in 0..j-1 ( 0 <= A[i,j] < n,  A[i,j] = H[i,j] modulo n )
+ (all other entries of A except j-1 defined by line above undefined)
 */
  {
  }
