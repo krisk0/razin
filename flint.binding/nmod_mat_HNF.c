06e55f70ba8481d6641d7e381812af77796b5332
Денис Крыськов <qauqe@list.ru>
2014-03-17 08:54:13 +0400

    old spec was stupid

--- flint.binding/nmod_mat_HNF.c
+++ flint.binding/nmod_mat_HNF.c
@@ -1,25 +1,18 @@
-#include <gmp.h>
-#include "flint.h"
-#include "nmod_mat.h"
-
-slong
+void
 nmod_mat_HNF(nmod_mat_t A)
 /*
 HNF: Hermite Normal Form as defined by W.Stein/C.Pernet
 
 Input data:
 ~~~~~~~~~~
-A: square non-singular over integers, A.r>0, det(A) is a divisor of n=mod
+A: square non-singular over integers, A.r>0, modulo n=A->mod.n is a multiple of 
+   determinant (these 2 numbres can be equal)
 
-let H be HNF of A over Z/nZ
+let H be HNF of A over Z
 
 Output data:
 ~~~~~~~~~~~
-if H is non-singular, modify A to be equal H and return -1
-if H is singular, return j such that H[j,j] is zero, and return A with
- the property
- for all i in 0..j-1 ( 0 <= A[i,j] < n,  A[i,j] = H[i,j] modulo n )
- (all other entries of A except j-1 defined by line above undefined)
+A modified so new A=H
 */
  {
  }
