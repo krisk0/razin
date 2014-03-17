d6bfdca0a87b971cd39fe0becdb17e72b6add1e1
Денис Крыськов <qauqe@list.ru>
2014-03-17 20:31:13 +0400

    now with spaces where they should be

--- flint.binding/delete_debug_C_code.sh
+++ flint.binding/delete_debug_C_code.sh
@@ -6,13 +6,17 @@
 m='#define NDEBUG 1'
 m="This C code is automagically generated out of nmod_mat_HNF-debug.c\n\n$m"
 
-perl -pe s/\\x0a/\\x1a/g nmod_mat_HNF-debug.c |
-perl -pe 's/#if .*?#endif/--go-away-debug-code/g' |
-perl -pe s/\\x1a/\\x0a/g | sed -e /--go-away-debug-code/d |
+perl -pe 's/ *#/#/g' nmod_mat_HNF-debug.c |
+perl -pe s/\\x0a/\\x1a/g |
+perl -pe 's/#if .*?#endif\x1a//g' |
+perl -pe s/\\x1a\\x1a\\x1astatic/\\x1a\\x1astatic/g |
+perl -pe s/\\x1a/\\x0a/g | 
 sed -e "s=#define BUG.*=//$m=" | sed -e 's:#define BUG.*::' |
-sed -e /MMUL/d | sed -e /MPLUS/d | sed -e '/delete me/d' |
-perl -pe 's:BUG 1\x0a:BUG 1:' \
+sed -e /MMUL/d | sed -e /MPLUS/d | sed -e '/delete me/d' \
 > nmod_mat_HNF.c
+
+#perl -pe s/\\x0a\\x0astatic/=static/g -i nmod_mat_HNF.c
+
 cat << EOF >> nmod_mat_HNF.c
 /*
 I will be greatly irritated if you tell me that this algorithm is wrong
