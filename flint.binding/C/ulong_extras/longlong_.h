52022d4406b057df2b7aa5ee0df5696e3509997c
Денис Крыськов <qauqe@list.ru>
2014-07-16 12:04:58 +0400

    modified comment

--- flint.binding/C/ulong_extras/longlong_.h
+++ flint.binding/C/ulong_extras/longlong_.h
@@ -9,7 +9,7 @@
 // Copyright Денис Крыськов 2014
 // Licence: GNU General Public License (GPL)
 
-// This file contains modified fragment of FLINT longlong.h, owned by W.Hart 
+// This file contains a modified fragment of FLINT longlong.h, owned by W.Hart 
 //  and F.Johansson
 
 
