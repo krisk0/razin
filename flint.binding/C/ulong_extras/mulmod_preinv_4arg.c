32348998d3686bf5908f563c5eaee426d648b17a
Денис Крыськов <qauqe@list.ru>
2014-07-16 15:46:29 +0400

    further clean n_mulmod_preinv_4arg()

--- flint.binding/C/ulong_extras/mulmod_preinv_4arg.c
+++ flint.binding/C/ulong_extras/mulmod_preinv_4arg.c
@@ -16,21 +16,19 @@ mp_limb_t
 n_mulmod_preinv_4arg(mp_limb_t a, mp_limb_t b, mp_limb_t n, mp_limb_t ninv)
 //castrated FLINT subroutine n_mulmod_preinv(): norm parameter removed
 {
-    mp_limb_t q0, q1, r, p_hi, p_lo;
+    mp_limb_t q0, q1, r, p;
 
     /* multiply */
-    umul_ppmm(p_hi, p_lo, a, b);
+    umul_ppmm(p, r, a, b);
     
     /* reduce mod n */
-    {
-        umul_ppmm(q1, q0, ninv, p_hi);
-        add_ssaaaa(q1, q0, q1, q0, p_hi, p_lo);
+    umul_ppmm(q1, q0, ninv, p);
+    add_ssaaaa(q1, q0, q1, q0, p, r);
 
-        r = (p_lo - (q1 + 1) * n);
+    r -= (q1 + 1) * n;
 
-        if (r >= q0)
-            r += n;
+    if (r >= q0)
+     r += n;
 
-        return (r < n ? r : r - n);
-    }
+    return (r < n ? r : r - n);
 }
