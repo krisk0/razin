eb060ad0a3a72ffc4b6365871cadceae044d20ce
Денис Крыськов <qauqe@list.ru>
2014-09-03 13:17:18 +0400

    new procedure transpose...

---
+++ flint.binding/C/nmod_mat/transpose_square_tgt_virgin.c
@@ -0,0 +1,22 @@
+// This program is part of RAZIN
+// Copyright Денис Крыськов 2014
+// Licence: GNU General Public License (GPL)
+
+#include <flint/nmod_mat.h>
+
+void
+nmod_mat_transpose_square_tgt_virgin(nmod_mat_t tgt,const nmod_mat_t sou)
+ {
+  slong n=tgt->r,i,j;
+  const mp_limb_t** sou_rows=sou->rows;
+  mp_limb_t* p;
+  mp_limb_t* q;
+  for(i=0;i<n;i++)
+   {
+    // transfer source row no. i
+    p=sou_rows[i];
+    q=tgt.entries+i;
+    for(j=0;j<n;j++,p++,q += n)
+     *q = *p;
+   }
+ }
