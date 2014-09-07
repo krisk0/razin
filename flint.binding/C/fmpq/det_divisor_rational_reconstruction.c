e8e465fc96a2464b0892c77165120376fc6047ed
Денис Крыськов <qauqe@list.ru>
2014-09-07 09:45:59 +0400

    fmpz_cmpabs_log2() test passed

--- flint.binding/C/fmpq/det_divisor_rational_reconstruction.c
+++ flint.binding/C/fmpq/det_divisor_rational_reconstruction.c
@@ -1,20 +1,306 @@
 // This program is part of RAZIN
+/******************************************************************************
+
+    Copyright (C) 2011 Fredrik Johansson
+
+******************************************************************************/
 // Copyright Денис Крыськов 2014
 // Licence: GNU General Public License (GPL)
 
+/*
+This file contains modified subroutine _fmpq_reconstruct_fmpz_2() owned by 
+ F.Johansson 
+
+Decreasing modulo/reusing discovered denominator trick learnt from solve1()
+ subroutine implemented by V.Shoup
+*/
+
+#define NDEBUG 0
+#include <assert.h>
+
 #include <flint/flint.h>
+#include <flint/fmpz.h>
+#include <flint/longlong.h>
+#include "../fmpz/fmpz_.h"
+
+#define ROT(u,v,t)   \
+    do { fmpz _t = *u; *u = *v; *v = *t; *t = _t; } while (0);
+
+#define MulMod( _t, _d, M )  \
+ {                          \
+  mpz_mul( _t, _t, _d );   \
+  mpz_mod( _t, _t, M );   \
+ }
+
+void __inline__ static
+MulMod_fmpz(mpz_t tgt,const fmpz_t sou,const mpz_t M)
+ {
+  if(fmpz_fits_si(sou))
+   mpz_mul_si(tgt, tgt, fmpz_get_si(sou));
+  else
+   mpz_mul(tgt,tgt,COEFF_TO_PTR(*sou));
+  mpz_mod(tgt,tgt,M);
+ }
+
+int __inline__ static
+maybe_decrease_M(mpz_t M,mp_limb_t p,mp_limb_t log2_N,fmpz_t D)
+// divide M by p so new M is still bigger than 2*N*D
+ {
+  int decreased=0;
+  mpz_t b; mpz_init(b);
+  fmpz_get_mpz(b,D);
+  mpz_mul_2exp(b,b,1+log2_N);
+  // M must be decreased iff b*p <= M
+  mpz_mul_ui(b,b,p);
+  // M must be decreased iff b <= M
+  while( mpz_cmp(b,M) <= 0 )
+   {
+    decreased=1;
+    mpz_divexact_ui(M,M,p);
+   }
+  mpz_clear(b);
+  return decreased;
+ }
+
+int __inline__ static
+fmpz_cmpabs_log2(const fmpz_t x,mp_limb_t y)
+ {
+  register slong xx=(slong)(*x);
+  if(!COEFF_IS_MPZ(xx))
+   return -1;          // |x|<2**64, 2**y>2**64
+  register mpz_ptr z=COEFF_TO_PTR(xx);
+  xx=mpz_size(z);      // xx>0
+  if(xx*FLINT_BITS < y)
+   return -1;          // x is too short, no need to further compare
+  --x;
+  if( xx*FLINT_BITS+1 > y )
+   return 1;           // even if senior limb is 1, x is larger
+  int lz;
+  count_leading_zeros(lz,mpz_getlimbn(z,xx));
+  xx=(xx*FLINT_BITS)+FLINT_BITS-lz;   // xx=|x| bit_length
+  if(xx <= y)
+   return -1;     // |x| fits y bits
+  if(xx-1 > y)
+   return 1;      // |x| > 2**y because its bit-length is y+2 or more
+  // Now either |x| is degree of 2, and result should be 0
+  //  or |x| is not degree of 2, and result should be 1
+  return !abs_x_is_degree_of_2(z);
+ }
+
+//fmpz_cmp_log2 is more expensive than fmpz_cmpabs_log2
+int __inline__ static
+fmpz_cmp_log2(const fmpz_t x,mp_limb_t y)
+ {
+  if(fmpz_cmp_ui(x,0)<=0)
+   return -1;                    // x<=0, 2**y>0
+  return fmpz_cmpabs_log2(x,y);
+ }
+
+// replace fmpz_cmp with fmpz_cmpabs because a is non-negative
+#define RR_tome0                   \
+    fmpz_t q, r, s, t;              \
+    int success = 0;                  \
+    if (fmpz_cmpabs_log2(a, N2) <= 0)    \
+    {                                 \
+        fmpz_set(n, a);              \
+        fmpz_one(d);                 \
+        return 1;                    \
+    }                                \
+    fmpz_sub(n, a, m);                \
+    if (fmpz_cmpabs_log2(n, N2) <= 0) \
+    {                                 \
+        fmpz_one(d);                 \
+        return 1;                    \
+    }                                \
+    fmpz_init(q);                     \
+    fmpz_init(r);                       \
+    fmpz_init(s);                          \
+    fmpz_init(t);                             \
+    fmpz_set(r, m); fmpz_zero(s);                \
+    fmpz_set(n, a); fmpz_one(d);                    \
+    while (fmpz_cmpabs_log2(n, N2) > 0)               \
+    {                                                   \
+        fmpz_fdiv_q(q, r, n);                             \
+        fmpz_mul(t, q, n); fmpz_sub(t, r, t); ROT(r, n, t); \
+        fmpz_mul(t, q, d); fmpz_sub(t, s, t); ROT(s, d, t); \
+    }                                                      \
+    if (fmpz_sgn(d) < 0)                                  \
+    {                                                    \
+        fmpz_neg(n, n);                                 \
+        fmpz_neg(d, d);                                \
+    }
+
+#define RR_tome1     \
+    fmpz_clear(q);   \
+    fmpz_clear(r);   \
+    fmpz_clear(s);   \
+    fmpz_clear(t);   \
+    return success;
+
+int reconstruct_rational_log2_plain(fmpz_t n, fmpz_t d,
+  const fmpz_t a, const fmpz_t m, mp_limb_t N2, const fmpz_t D,
+  int skip_check)
+/*
+like _fmpq_reconstruct_fmpz_2(), but 
+ * numerator bound is logarithmic
+ * optionally skip check
+ * dont run gcd algorithm when performing check 
+*/
+ {
+  RR_tome0
+  if (fmpz_cmp(d, D) <= 0)
+   {
+    if(skip_check)
+     success=1;
+    else
+     {
+      /* 
+       a=n/d modulo m  iff gcd(n,d)=1. But it should be faster to directly
+       compare a*d to n
+      */
+      fmpz_mul(s, a,d);
+      fmpz_mod(t, s,m);
+      if(fmpz_cmp_ui(n,0)>0)
+       success = !fmpz_cmp(n,t);
+      else
+       {
+        fmpz_mod(s, n,m);
+        success = !fmpz_cmp(s,t);
+       }
+     }
+   }
+  RR_tome1
+ }
+
+int reconstruct_rational_log2_log2(fmpz_t n, fmpz_t d,
+  const fmpz_t a, const fmpz_t m, mp_limb_t N2, mp_limb_t D2,
+  int skip_check)
+// like reconstruct_rational_log2_plain(), but denominator bound is logarithmic
+ {
+  RR_tome0
+  if (fmpz_cmpabs_log2(d, D2) <= 0)  // d is positive, using cheaper
+   {                                 //  fmpz_cmpabs_log2
+    if(skip_check)
+     success=1;
+    else
+     {
+      fmpz_mul(s, a,d);
+      fmpz_mod(t, s,m);
+      if(fmpz_cmp_ui(n,0)>0)
+       success = !fmpz_cmp(n,t);
+      else
+       {
+        fmpz_mod(s, n,m);
+        success = !fmpz_cmp(s,t);
+       }
+     }
+   }
+  RR_tome1
+ }
+
+#undef RR_tome1
+#undef RR_tome0
+
+int __inline__ static
+reconstruct_rational_take_denominator(fmpz_t d,mpz_t aa,const mpz_t mm,
+  mp_limb_t log2_N, fmpz_t D, int skip_check)
+// perform rational reconstruction, drop numerator, return denominator
+ {
+  fmpz_t m; fmpz_init_set_readonly(m, mm); 
+  fmpz_t a; fmpz_init_set_readonly(a, aa); 
+  fmpz_t n; fmpz_init(n);
+  int r=reconstruct_rational_log2_plain(n,d, a,m, log2_N,D, skip_check);
+  fmpz_clear(n);
+  fmpz_clear_readonly(a);
+  fmpz_clear_readonly(m);
+  return r;
+ }
+
+int __inline__ static
+reconstruct_rational_take_denominator_log2(fmpz_t d,mpz_t aa,const mpz_t mm,
+  mp_limb_t log2_N, mp_limb_t log2_D, int skip_check)
+// perform rational reconstruction, drop numerator, return denominator
+ {
+  fmpz_t m; fmpz_init_set_readonly(m, mm); 
+  fmpz_t a; fmpz_init_set_readonly(a, aa); 
+  fmpz_t n; fmpz_init(n);
+  int r=reconstruct_rational_log2_log2(n,d, a,m, log2_N,log2_D, skip_check);
+  fmpz_clear(n);
+  fmpz_clear_readonly(a);
+  fmpz_clear_readonly(m);
+  return r;
+ }
 
 void
 det_divisor_rational_reconstruction(mpz_t d,mpz_ptr x,mpz_t M,mp_limb_t p,
   slong n,mp_limb_t log2_N, mp_limb_t log2_D)
 /*
-M=modulo=p**k
-n: length of x
+M=modulo=p**k, for some integer k
 log2_N: log2(upper bound on numerators)
 log2_D: log2(upper bound on denominators)
+x[i] is in range 0..M-1, for i in 0..n-1
+
+Algorithm behind this subroutine inspired by Victor Shoup solve1() subroutine 
+ found in mat_ZZ.c (which is part of NTL)
 */
  {
   mpz_set_ui(d,1);
-  flint_printf("det_divisor_rational_reconstruction not imple\n");
-  abort();
+  mpz_t d_mod_M; mpz_init_set_ui(d_mod_M,1);
+  slong i;
+  int M_modified=0,rc;
+  mpz_ptr xI;
+  fmpz_t denom_i; fmpz_init(denom_i);
+  fmpz_t D; fmpz_init(D); // D=0
+  for(i=0; i<n; i++)
+   {
+    xI = x+i;
+    if(M_modified)
+     mpz_mod(xI,xI,M);
+    if( mpz_cmp_ui(d_mod_M,1) )
+     MulMod( xI, d_mod_M, M );
+    assert( mpz_cmp_ui(xI,0) >= 0 );
+    // if not debugging, skip unnecessary check in rational rec. subroutine
+    if( fmpz_cmp_ui(D,0) )
+     rc=reconstruct_rational_take_denominator_log2(denom_i, xI, M, log2_N, 
+      log2_D, NDEBUG);
+    else
+     rc=reconstruct_rational_take_denominator(denom_i, xI, M, log2_N, D, 
+      NDEBUG);
+    if(!rc)
+     {
+      flint_printf("Exception (det_divisor_rational_reconstruction): "
+                     "Rational reconstruction failed.\n");
+      abort();
+     }
+    if( fmpz_cmp_ui(denom_i,1) )
+     {
+      mul_mpz_fmpz( d, denom_i );
+      if( i != n-1 )
+       {
+        // Decrease denominator bound, maybe decrease M
+        if( !fmpz_cmp_ui(D,0) )
+         {
+          fmpz_set_ui(D,1); fmpz_mul_2exp(D,D,log2_D);
+         }
+        fmpz_fdiv_q(D, D, denom_i);
+        M_modified=maybe_decrease_M(M,p,log2_N,D);
+        // multiply d_mod_M by denom_i and reduce result modulo M
+        if(M_modified)
+         {
+          mpz_mod(d_mod_M,d_mod_M,M);
+          fmpz_mod_2arg(denom_i,M);
+         }
+        MulMod_fmpz(d_mod_M, denom_i, M);
+       }
+     }
+   }
+  fmpz_clear(D);
+  fmpz_clear(denom_i);
+  mpz_clear(d_mod_M);
+  #if NDEBUG==0
+   flint_printf("det_divisor_rational_reconstruction(): check positive\n");
+  #endif
  }
+
+#undef MulMod
+#undef NDEBUG
---
+++ flint.binding/test_fmpz_cmpabs_log2.c
@@ -0,0 +1,91 @@
+// This program is part of RAZIN
+// Copyright Денис Крыськов 2014
+// Licence: GNU General Public License (GPL)
+
+// This program tests function fmpz_cmpabs_log2() against macro fmpz_cmpabs()
+
+// To compile: gcc -O2 -march=native -lflint -lgmp
+
+#include <assert.h>
+#include "C/fmpq/det_divisor_rational_reconstruction.c"
+
+flint_rand_t rst;
+
+void test2(mp_limb_t log2,const fmpz_t b,const fmpz_t n)
+ {
+  int r0=fmpz_cmpabs(n,b);
+  int r1=fmpz_cmpabs_log2(n,log2);
+  if( r0>0 )
+   assert( r1>0 );
+  if( r0<0 )
+   assert( r1<0 );
+  assert( 0 == r1 );
+ }
+
+void test1(mp_limb_t log2,const fmpz_t b,const fmpz_t n)
+ {
+  test2(log2,b,n);
+  if(fmpz_cmp_ui(n,0))
+   {
+    fmpz_t m; fmpz_init_set(m,n);
+    fmpz_neg(m,m);
+    test2(log2,b,m);
+    fmpz_clear(m);
+   }
+ }
+
+void
+test1_ui(mp_limb_t log2,const fmpz_t b,mp_limb_t mm)
+ {
+  fmpz_t m; fmpz_init_set_ui(m,mm);
+  test1(log2,b,m);
+  fmpz_clear(m);
+ }
+
+void
+test0(slong b)
+ {
+  fmpz_t M; fmpz_init_set_ui(M,1);
+  fmpz_mul_2exp(M,M,(ulong)b);
+  test1_ui(b,M,0);
+  test1_ui(b,M,1);
+  test1_ui(b,M,(mp_limb_t)-1);
+  test1(b,M,M);                      // M
+  fmpz_t n; fmpz_init_set(n,M);
+  fmpz_add_ui(n,M,1);
+  test1(b,M,n);                      // M+1
+  fmpz_sub_ui(n,M,1);
+  test1(b,M,n);                      // M-1
+  slong i;
+  fmpz_t m; fmpz_init(m);
+  fmpz_t Mhalf; fmpz_init(Mhalf);
+  fmpz_fdiv_q_2exp(Mhalf,M,1);
+  for(i=100;i--;)
+   {
+    fmpz_randm(m,rst,n);                      // m = random(M-1)
+    test1(b,M,m);
+    fmpz_add(m,m,Mhalf);                     // m+M/2
+    test1(b,M,m);
+   }
+  fmpz_mul_2exp(m,M,1);
+  test1(b,M,m);                      // M<<1
+  fmpz_mul_2exp(m,m,1);
+  test1(b,M,m);                      // M<<2
+  fmpz_mul_2exp(m,m,1);
+  test1(b,M,m);                      // M<<3
+  fmpz_mul(m,m,M);
+  test1(b,M,m);                      // M*(M<<3)
+  fmpz_clear(Mhalf);
+  fmpz_clear(m);
+  fmpz_clear(n);
+  fmpz_clear(M);
+ }
+
+int main()
+ {
+  flint_randinit(rst);
+  slong i;
+  for(i=FLINT_BITS;i<4*FLINT_BITS+5;i++)
+   test0(i);
+  flint_printf("Test passed\n");
+ }
