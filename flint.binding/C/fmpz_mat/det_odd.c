a958c5fd512856e8a1c5c7a0124b1ea7757fb20f
Денис Крыськов <qauqe@list.ru>
2014-09-12 12:18:02 +0400

    add calls to 2 non-existing-yet subroutines

--- flint.binding/C/fmpz_mat/det_odd.c
+++ flint.binding/C/fmpz_mat/det_odd.c
@@ -2,16 +2,20 @@
 // Copyright Денис Крыськов 2014
 // Licence: GNU General Public License (GPL)
 
-#undef NDEBUG
-#include <assert.h>
 #include <flint/flint.h>
 #include "../fmpz_mat/fmpz_mat_.h"
 
-void fmpz_mat_det_odd(fmpz_t r,const fmpz_mat_t A)
+void fmpz_mat_det_odd(fmpz_t r,const fmpz_mat_t a)
  {
   flint_printf("fmpz_mat_det_odd() A:\n");
-  fmpz_mat_print_pretty(A);
+  fmpz_mat_print_pretty(a);
   abort();
+  mp_limb_t det_mod_T;
+  mpz_t m; mpz_init(m);
+  //mp_limb_t hb=fmpz_mat_det_divisor_odd(m, &det_mod_T, a);
+  //fmpz_mat_det_modular_given_divisor_4arg(m, hb, det_mod_T, a);
+  fmpz_set_mpz(r,m);
+  mpz_clear(m);
  }
 
 #undef NDEBUG
