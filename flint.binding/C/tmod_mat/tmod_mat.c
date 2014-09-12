1e140e20386dd2c1288e973310bac1df796c2e2b
Денис Крыськов <qauqe@list.ru>
2014-09-12 16:38:25 +0400

    expose t_invmod()

--- flint.binding/C/tmod_mat/tmod_mat.c
+++ flint.binding/C/tmod_mat/tmod_mat.c
@@ -6,6 +6,7 @@
 #include <flint/flint.h>
 #include <flint/ulong_extras.h>
 #include <flint/fmpz.h>
+#include "tmod_mat_.h"
 
 // Subroutines in this file are machine-dependent and known to work on amd64
 //  with FLINT ver 2.4.1 and 2.4.3
@@ -88,37 +89,6 @@ This function calculates f modulo 2**64 on amd64, at least for FLINT ver
   m=flint_mpz_get_ui(COEFF_TO_PTR(n));
   m *= (mp_limb_t)fmpz_sgn(f);
   return m;
- }
-
-static mp_limb_t inv_mod__2_64__tab[13]={
- 1,0xAAAAAAAAAAAAAAAB,0xCCCCCCCCCCCCCCCD,0x6DB6DB6DB6DB6DB7,0x8E38E38E38E38E39
- ,0x2E8BA2E8BA2E8BA3,0x4EC4EC4EC4EC4EC5,0xEEEEEEEEEEEEEEEF,0xF0F0F0F0F0F0F0F1,
- 0x86BCA1AF286BCA1B,0xCF3CF3CF3CF3CF3D,0xD37A6F4DE9BD37A7,0x8F5C28F5C28F5C29};
-
-static __inline__ mp_limb_t 
-t_invmod(mp_limb_t a)
- {
-  #define m 64
-  mp_limb_t s;
-  mp_limb_t t=a-1;
-  if(t<26)
-   return inv_mod__2_64__tab[ t>>1 ];
-  // inspired by public domain code created by Jean-Guillaume Dumas
-  count_trailing_zeros(s,t);
-  t >>= s; 
-  //assert( a == (1<<s)*t + 1 );
-  //assert( t&1 );
-  mp_limb_t U=2-a;
-  mp_limb_t amone=a-1;
-  long i,i_max;
-  i_max=m/s;
-  for(i=1;i<i_max;i <<= 1)
-   {
-    amone *= amone;
-    U *= amone+1;
-   }
-  #undef m
-  return U;
  }
 
 #include "tmod_mat_PLU.c"
--- flint.binding/C/tmod_mat/tmod_mat_.h
+++ flint.binding/C/tmod_mat/tmod_mat_.h
@@ -2,6 +2,9 @@
 // Copyright Денис Крыськов 2014
 // Licence: GNU General Public License (GPL)
 
+// t_invmod() macro uses a piece of public domain code owned 
+//  by Jean-Guillaume Dumas
+
 #ifndef TMOD_MAT__H
 #define TMOD_MAT__H
 
@@ -17,5 +20,37 @@ typedef struct
 tmod_mat_struct;
 typedef tmod_mat_struct tmod_mat_t[1];
 #define tmod_mat_entry(mat,i,j) ((mat)->rows[(i)][(j)])
+
+// TODO: this number-oriented subroutine should be in /ulong_extras
+static mp_limb_t inv_mod__2_64__tab[13]={
+ 1,0xAAAAAAAAAAAAAAAB,0xCCCCCCCCCCCCCCCD,0x6DB6DB6DB6DB6DB7,0x8E38E38E38E38E39
+ ,0x2E8BA2E8BA2E8BA3,0x4EC4EC4EC4EC4EC5,0xEEEEEEEEEEEEEEEF,0xF0F0F0F0F0F0F0F1,
+ 0x86BCA1AF286BCA1B,0xCF3CF3CF3CF3CF3D,0xD37A6F4DE9BD37A7,0x8F5C28F5C28F5C29};
+
+static __inline__ mp_limb_t 
+t_invmod(mp_limb_t a)
+ {
+  #define m 64
+  mp_limb_t s;
+  mp_limb_t t=a-1;
+  if(t<26)
+   return inv_mod__2_64__tab[ t>>1 ];
+  // inspired by public domain code created by Jean-Guillaume Dumas
+  count_trailing_zeros(s,t);
+  t >>= s; 
+  //assert( a == (1<<s)*t + 1 );
+  //assert( t&1 );
+  mp_limb_t U=2-a;
+  mp_limb_t amone=a-1;
+  long i,i_max;
+  i_max=m/s;
+  for(i=1;i<i_max;i <<= 1)
+   {
+    amone *= amone;
+    U *= amone+1;
+   }
+  #undef m
+  return U;
+ }
 
 #endif
