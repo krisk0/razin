07ecfffa9ea09c88e2379d44f19e012424693e37
Денис Крыськов <qauqe@list.ru>
2014-09-12 16:39:13 +0400

    un-comment call to ..._det_modular_given_divisor_4arg()

--- flint.binding/C/fmpz_mat/det_odd.c
+++ flint.binding/C/fmpz_mat/det_odd.c
@@ -13,7 +13,8 @@ void fmpz_mat_det_odd(fmpz_t r,const fmpz_mat_t a)
   mp_limb_t det_mod_T;
   mpz_t m; mpz_init(m);
   //mp_limb_t hb=fmpz_mat_det_divisor_odd(m, &det_mod_T, a);
-  //fmpz_mat_det_modular_given_divisor_4arg(m, hb, det_mod_T, a);
+  if( hb )
+   fmpz_mat_det_modular_given_divisor_4arg(m, hb, det_mod_T, a);
   fmpz_set_mpz(r,m);
   mpz_clear(m);
  }
