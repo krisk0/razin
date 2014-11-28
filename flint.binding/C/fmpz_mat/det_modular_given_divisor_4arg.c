4a613be436d9cd23a388c8b3f5eadd56a25c040a
Денис Крыськов <qauqe@list.ru>
2014-11-28 13:24:45 +0300

    speed-up for small hb

--- flint.binding/C/fmpz_mat/det_modular_given_divisor_4arg.c
+++ flint.binding/C/fmpz_mat/det_modular_given_divisor_4arg.c
@@ -117,6 +117,31 @@ _20140912_no_CRT(mpz_t r,mp_limb_t det_mod_t)
    }
  }
 
+static __inline__ mp_limb_t
+_20141128_prime_pow_1(p_k_pk_t* pp,nmod_t* mod,const mpz_t divisor)
+ {
+  mp_limb_t r,t,r_mod_p;
+  pp->k=1;
+  while(1)
+   {
+    pp->p = n_nextprime(pp->p, 0); // in FLINT 2.4.4 this is prime
+    r=mpz_fdiv_ui( divisor, pp->p );
+    r_mod_p=r % pp->p;
+    if(r_mod_p)
+     {
+      count_leading_zeros( t, pp->p_deg_k=pp->p );
+      mod->n = pp->p_deg_k << t;
+      invert_limb(mod->ninv, mod->n);
+      #if SPEEDUP_NMOD_RED3
+       t = - mod->n;
+       mod->norm = n_mulmod_preinv_4arg( t,t, mod->n,mod->ninv );
+      #endif
+      // save 44 ticks by re-using r % pp->p
+      return inv_mod_pk_4arg(r,r_mod_p,pp[0],mod[0]);
+     }
+   }
+ }
+
 void
 fmpz_mat_det_modular_given_divisor_4arg(mpz_t r, mp_limb_t hb,
   mp_limb_t det_mod_T, const fmpz_mat_t A)
@@ -129,13 +154,11 @@ fmpz_mat_det_modular_given_divisor_4arg(mpz_t r, mp_limb_t hb,
   {
    // looks like small primes are better than large primes for this subroutine
    // TODO: use small primes produced with n_nextprime() instead of big primes
-   // TODO: use degree 1 of small prime if hb is small
    #if LOUD_DET_BOUND
     gmp_printf("fmpz_mat_det_modular_given_divisor_4arg(): hb=%Md\n",hb);
     slong primes_used=0;
    #endif
    p_k_pk_t pp; pp.p=0;
-   n_primes_rev_t iT;
  
    fmpz_t x,xnew,prod;
    _CRT_data_init_20140912(x,xnew,prod,det_mod_T,r);
@@ -144,24 +167,41 @@ fmpz_mat_det_modular_given_divisor_4arg(mpz_t r, mp_limb_t hb,
    mp_limb_t* scratch=flint_malloc( 4*(n-4)*sizeof(mp_limb_t) );
    nmod_mat_t Amod;
    nmod_mat_init_square_2arg(Amod,n);
-   
-   for(;;)
+
+   if( hb < 2*FLINT_BITS ) // maybe one small prime will be enough?
     {
-     mp_limb_t divisor_inv=_choose_prime_20140912(&pp, &Amod->mod, iT, r);
+     pp.p=UWORD(1)<<(hb-FLINT_BITS);
+     mp_limb_t divisor_inv=_20141128_prime_pow_1(&pp, &Amod->mod, r);
+     #if LOUD_DET_BOUND
+      gmp_printf("fmpz_mat_det_modular_given_divisor_4arg():"
+                 " using power 1 of prime %Md\n",pp.p);
+      primes_used=1;
+     #endif
      fmpz_mat_get_nmod_mat(Amod, A);
      mp_limb_t xmod=nmod_mat_det_mod_pk_4block(Amod,pp,scratch);
      xmod=n_mulmod2_preinv(xmod,divisor_inv, Amod->mod.n,Amod->mod.ninv);
      fmpz_CRT_ui(xnew, x,prod, xmod,pp.p_deg_k, 1);
-     fmpz_mul_ui(prod, prod, pp.p_deg_k);
-     #if LOUD_DET_BOUND
-      primes_used++;
-     #endif
-     if(cmp_positive_log2(prod,hb) >= 0)
-      break;
-     fmpz_set(x, xnew);
     }
- 
-   n_primes_rev_clear(iT);
+   else
+    {
+     n_primes_rev_t iT;
+     for(;;)
+      {
+       mp_limb_t divisor_inv=_choose_prime_20140912(&pp, &Amod->mod, iT, r);
+       fmpz_mat_get_nmod_mat(Amod, A);
+       mp_limb_t xmod=nmod_mat_det_mod_pk_4block(Amod,pp,scratch);
+       xmod=n_mulmod2_preinv(xmod,divisor_inv, Amod->mod.n,Amod->mod.ninv);
+       fmpz_CRT_ui(xnew, x,prod, xmod,pp.p_deg_k, 1);
+       fmpz_mul_ui(prod, prod, pp.p_deg_k);
+       #if LOUD_DET_BOUND
+        primes_used++;
+       #endif
+       if(cmp_positive_log2(prod,hb) >= 0)
+        break;
+       fmpz_set(x, xnew);
+      }
+     n_primes_rev_clear(iT);
+    }
    nmod_mat_clear(Amod);
    free(scratch);
    mpz_fmpz_mul_det_2arg(r,xnew);
